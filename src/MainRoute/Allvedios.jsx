d88c8a1ab5bf2706f0cc2145eba2659081bf2e78
Jahongir <akhrasmiy@gmail.com>
2023-05-07 15:57:48 +0500

    input allvedio

--- src/MainRoute/Allvedios.jsx
+++ src/MainRoute/Allvedios.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import Cart from '../components/Cart/Cart'
 import Sitebar from '../layout/Sitebar'
 import Navvedio from '../sitebarRoute/Navvedio'
+import { Input } from 'antd';
 function Allvedios() {
     return (
-        <div style={{ display: "flex"}}>
-            <div className='fife' style={{ display: "flex", flexWrap: "wrap",justifyContent:"space-around"}}>
-                <Cart />
-                <Cart />
-                <Cart />
-                <Cart />
+        <div style={{ display: "flex" }}>
+            <div>
+            <Input placeholder="Basic usage" />;
+                <div className='fife' style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}>
+
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                </div>
             </div>
+
 
             <Navvedio />
         </div>
