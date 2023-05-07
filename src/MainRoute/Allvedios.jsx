fd7d0971af2b3369822f4eb10343cb5dacc7b90d
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2023-05-07 19:15:06 +0500

    styless

--- src/MainRoute/Allvedios.jsx
+++ src/MainRoute/Allvedios.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Cart from "../components/Cart/Cart";
 import Sitebar from "../layout/Sitebar";
 import Navvedio from "../sitebarRoute/Navvedio";
+import search from '../imgs/search.png';
 import { Input } from "antd";
 import './style.css';
 function Allvedios() {
   return (
     <div className="main-page" >
       <div className="w100">
-        <Input placeholder="Basic usage" />
+        {/* <Input placeholder="Basic usage" /> */}
+        <div className="search-div">
+          <input type="search" className="search-main" placeholder="search..." />
+          <img src={search} alt="" />
+        </div>
         <div
           className="fife main-content"
           style={{
--- src/sitebarRoute/Navvedio.jsx
+++ src/sitebarRoute/Navvedio.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./index.css";
 import homeSidebar1 from "../imgs/sidebar1.png";
 import homeSidebar2 from "../imgs/sidebar2.png";
+import homeSidebar3 from "../imgs/sidebar3.png";
+import homeSidebar4 from "../imgs/sidebar4.png";
 import SidebarCart from "../components/SidebarCart/SidebarCart";
 const Navvedio = () => {
   return (
-    <div className="Nav">
+    <div className="Nav sidebar-main-content">
       <h2>Olingan darslar - 2 ta</h2>
       <div className="sidebar-line"></div>
       <div className="sidebar-bought-course">
@@ -18,6 +20,16 @@ const Navvedio = () => {
           text={`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`}
         />
       </div>
+      <h2 className="saqlanganlar">Saqlanganlar - 2 ta</h2>
+      <div className="sidebar-line"></div>
+      <SidebarCart
+          img={homeSidebar3}
+          text={`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`}
+        />
+        <SidebarCart
+          img={homeSidebar4}
+          text={`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`}
+        />
     </div>
   );
 };
