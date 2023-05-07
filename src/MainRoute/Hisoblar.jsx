5769aa51dca15a0ff9754b8f5c0c3cfe05178299
Jahongir <akhrasmiy@gmail.com>
2023-05-07 15:49:05 +0500

    hisoblar

--- src/MainRoute/Hisoblar.jsx
+++ src/MainRoute/Hisoblar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Baystudy from '../sitebarRoute/Baystudy'
-
+let money =650000
+money=money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+let hisobraqam=1234567890123456
+hisobraqam= hisobraqam.toString().replace(/\B(?=(\d{4})+(?!\d))/g, ' ')
 function Hisoblar() {
     return (
         <div style={{ display: "flex"}}>
-            <div className='fife'>balans</div>
+            <div className='fife'>
+                <p style={{fontSize:22}}>Hisobdagi pul miqdori:</p>
+                <p style={{fontSize:45}}>{money}UZS</p>
+                <p>Hisob raqam:</p>
+                <p style={{fontSize:45}}>{hisobraqam}</p>
+            </div>
             <Baystudy />
         </div>
     )
