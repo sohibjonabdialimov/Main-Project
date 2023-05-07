b2819fe93933d22bd9dc25854f330b067c038642
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2023-05-07 16:37:59 +0500

    okkk

--- src/MainRoute/Allvedios.jsx
+++ src/MainRoute/Allvedios.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import Cart from '../components/Cart/Cart'
-import Sitebar from '../layout/Sitebar'
-import Navvedio from '../sitebarRoute/Navvedio'
-import { Input } from 'antd';
+import React from "react";
+import Cart from "../components/Cart/Cart";
+import Sitebar from "../layout/Sitebar";
+import Navvedio from "../sitebarRoute/Navvedio";
+import { Input } from "antd";
 function Allvedios() {
-    return (
-        <div style={{ display: "flex" }}>
-            <div>
-            <Input placeholder="Basic usage" />;
-                <div className='fife' style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}>
-
-                    <Cart />
-                    <Cart />
-                    <Cart />
-                    <Cart />
-                </div>
-            </div>
-
-
-            <Navvedio />
+  return (
+    <div style={{ display: "flex" }}>
+      <div>
+        <Input placeholder="Basic usage" />;
+        <div
+          className="fife"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
+        >
+          <Cart />
+          <Cart />
+          <Cart />
+          <Cart />
         </div>
+      </div>
 
-    )
+      <Navvedio />
+    </div>
+  );
 }
 
-export default Allvedios
+export default Allvedios;
