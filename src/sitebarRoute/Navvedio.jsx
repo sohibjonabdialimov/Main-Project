0c84205104470cb491120d547ddd2162fd1d578a
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2023-05-07 16:09:50 +0500

    styles

--- src/sitebarRoute/Navvedio.jsx
+++ src/sitebarRoute/Navvedio.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import homeSidebar1 from '../imgs/sidebar1.png'
 const Navvedio = () => {
   return (
     <div className='Nav'>
       <h2>Olingan darslar - 2 ta</h2>
-      <div>
-        <img src="https://picsum.photos/200/200" alt="" />
+      <div className='sidebar-line'></div>
+      <div className='sidebar-component'>
+        <img src={homeSidebar1} alt="" />
       </div>
     </div>
   )
