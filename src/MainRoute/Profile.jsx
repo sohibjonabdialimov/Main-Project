c0d8efc2170ed6caa7371c806dffeeb4056add3e
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2023-05-09 14:23:28 +0500

    every users styles

--- src/MainRoute/Profile.jsx
+++ src/MainRoute/Profile.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import Obuna from '../sitebarRoute/Obuna';
-import opacha from "../imgs/big-logo.png"
-import Userprofile from '../components/userprofile';
+import React from "react";
+import Obuna from "../sitebarRoute/Obuna";
+import opacha from "../imgs/big-logo.png";
+import Userprofile from "../components/userprofile";
 
 function Profile() {
-    let name = "name"
-    let surname = "surname"
-    let username = "username"
-    let email = "username@email.com"
+  let name = "name";
+  let surname = "surname";
+  let username = "username";
+  let email = "username@email.com";
 
-    return (
-        <div className='main-page'>
-            <div className='fife' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                
-                    <div style={{ textAlign: "center" }}>
-                        <img src={opacha} alt="" />
-                        <h2>{name + " " + surname}</h2>
-                        <p>{username}</p>
-                        <p>{email}</p>
-                    </div>
-                
-            </div>
-            <Obuna />
+  return (
+    <div className="main-page">
+      <div
+        className="fife w100"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ textAlign: "center" }}>
+          <img src={opacha} alt="" />
+          <h2>{name + " " + surname}</h2>
+          <p>{username}</p>
+          <p>{email}</p>
         </div>
-    )
+      </div>
+      <Obuna />
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,32 +1,30 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 import "./index.css";
 const Cart = (props) => {
-    const navigate = useNavigate();
-    const { img, name, autorId } = props.cart;
-    let kursId = props.cart.kursId;
-    console.log(kursId);
-    return (
-        <div
-            onClick={() => {
-                navigate("/kurs/" + kursId);
-            }}
-        >
-            <div className="main-cart">
-                <img src={props.cart.img} alt="" />
-                <div className="cart-desc">
-                    <h3>{props.cart.name}</h3>
-                    <p>{props.cart.text}</p>
-                    <div className="desc">
-                        <img src={props.cart.smallImg} alt="" />
-                        <span>{props.cart.author}</span>
-                    </div>
-                </div>
-            </div>
+  const navigate = useNavigate();
+  const { img, name, autorId } = props.cart;
+  let kursId = props.cart.kursId;
+  return (
+    <div
+      onClick={() => {
+        navigate("/kurs/" + kursId);
+      }}
+    >
+      <div className="main-cart">
+        <img src={props.cart.img} alt="" />
+        <div className="cart-desc">
+          <h3>{props.cart.name}</h3>
+          <p>{props.cart.text}</p>
+          <div className="desc">
+            <img src={props.cart.smallImg} alt="" />
+            <span>{props.cart.author}</span>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
