288781b7a57b79da0cf246dac8f295de26e56fb5
Jahongir <akhrasmiy@gmail.com>
2023-06-19 18:04:47 +0500

    m

--- src/teacher/components/Userprofile.jsx
+++ src/teacher/components/Userprofile.jsx
@@ -3,41 +3,41 @@ import opacha from "../../imgs/user-logo.png"
 import "./style.css"
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-function deleteplatforma(url){
-    try {
-      if(url.includes("platforma")){
-        url=url.split("/")
-        let res=""
-        for(let i=2;i<url.length;i++){
-          res+="/"+url[i]
-        }
-        return(res)
+function deleteplatforma(url) {
+  try {
+    if (url.includes("platforma")) {
+      url = url.split("/")
+      let res = ""
+      for (let i = 2; i < url.length; i++) {
+        res += "/" + url[i]
       }
-      return "/"+url
-    } catch (error) {
-      console.log(error)
+      return (res)
     }
+    return "/" + url
+  } catch (error) {
+    console.log(error)
   }
+}
 function TeachUserprofile() {
-    const navigate = useNavigate();
-  const [profile,setProfile]=useState([]);
-  useEffect(()=>{
-    axios.get("http://165.232.127.62:5001/teacherme/",{
+  const navigate = useNavigate();
+  const [profile, setProfile] = useState([]);
+  useEffect(() => {
+    axios.get("http://165.232.127.62:5001/teacherme/", {
       headers: {
         Authorization: localStorage.getItem("token"),
       },
-    }).then((res)=>{
+    }).then((res) => {
       setProfile(res.data)
     })
-  },[])
-    return (
-        <div>
-            <div className='userprofile' style={{ textAlign: "center",paddingTop:15 }}>
-                <img src={"http://165.232.127.62:5001"+deleteplatforma(profile.path)} alt="" />
-                <h2>{profile.fullname}</h2>
-            </div>
-        </div>
-    )
+  }, [])
+  return (
+    <div>
+      <div className='userprofile' style={{ textAlign: "center", paddingTop: 15 }}>
+        <img src={"http://165.232.127.62:5001" + deleteplatforma(profile.path)} alt="" />
+        <h2>{profile.fullname}</h2>
+      </div>
+    </div>
+  )
 }
 
 export default TeachUserprofile
